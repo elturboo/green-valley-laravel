b6fc7ca7c5d4cbcb086a72d7c0f773b610b89f12
elturboo <ma7moud.3bdelmo7sen@gmail.com>
2023-04-11 15:28:38 +0300

    update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -340,8 +340,8 @@ class ComposerStaticInitb8bf0f98ab7b84b1624a1c212a6b4119
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -511,6 +511,7 @@ class ComposerStaticInitb8bf0f98ab7b84b1624a1c212a6b4119
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
+        'App\\Http\\Middleware\\LangManger' => __DIR__ . '/../..' . '/app/Http/Middleware/LangManger.php',
         'App\\Http\\Middleware\\PreventRequestsDuringMaintenance' => __DIR__ . '/../..' . '/app/Http/Middleware/PreventRequestsDuringMaintenance.php',
         'App\\Http\\Middleware\\RedirectIfAuthenticated' => __DIR__ . '/../..' . '/app/Http/Middleware/RedirectIfAuthenticated.php',
         'App\\Http\\Middleware\\TrimStrings' => __DIR__ . '/../..' . '/app/Http/Middleware/TrimStrings.php',
